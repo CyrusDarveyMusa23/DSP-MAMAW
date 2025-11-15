9260327969ce9006df22023aa750cfb1d685541d
Alexandra Jagonos <jagonosalexandra@gmail.com>
2025-11-15 12:21:49 +0800

    Add filtered data

--- filtered data/test.m
+++
@@ -1,6 +0,0 @@
-file = 'C:\Users\User\Desktop\DSP MAMAW\DSP-MAMAW\data\ppg_dalia_all_activities_data.mat';
-whos('-file', file)
-
-S = load(file);
-rec = S.data(1);      % inspect subject 1
-fieldnames(rec)
